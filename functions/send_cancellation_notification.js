76324c4d231d5028645365a2bd1c5ff0b0ecae84
alessandrol <alessandrol@spotify.com>
2021-09-12 16:35:00 +0200

    add functions to send notification of cancellation

---
+++ functions/send_cancellation_notification.js
@@ -0,0 +1,48 @@
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+
+exports.sendCancellationNotification = functions.https.onRequest((req, res) => {
+    let matchId = req.body.matchId
+    console.log("fetching subs for match: " + matchId)
+
+    runFunction(matchId)
+        .then(() => res.status(200).send())
+        .catch((e) => res.status(500).send(e))
+});
+
+const runFunction = async function (matchId) {
+    let subs = Array.from(await getLatestSubscriptionsPerUser(matchId));
+    console.log("found " + subs.length + " subscriptions");
+    let goingUsers = subs.filter(s => s.status === "going").map(s => s.userId);
+    console.log(goingUsers);
+    let tokens = (await Promise.all(goingUsers.map(async (u) => await getUserTokens(u)))).flat();
+    await sendNotificationToTokens(tokens);
+}
+
+const getLatestSubscriptionsPerUser = async function (matchId) {
+    const qs = await admin.firestore().collection("matches/" + matchId + "/subscriptions").get();
+    const documentsData = qs.docs.map(doc => doc.data());
+
+    const latestSubs = new Map();
+    let subscriptions;
+    let current;
+    for (let i = 0; i < documentsData.length; i++) {
+        subscriptions = documentsData[i];
+        current = latestSubs.get(subscriptions.userId);
+        if (current == null || subscriptions.createdAt > current.createdAt) {
+            latestSubs.set(subscriptions.userId, subscriptions);
+        }
+    }
+    return latestSubs.values();
+}
+
+const getUserTokens = async function (userId) {
+    const ds = await admin.firestore().doc("users/" + userId).get();
+    return ds.data().tokens
+}
+
+const sendNotificationToTokens = async function (tokens) {
+    console.log("sending notifications to " + tokens.length + " devices");
+    console.log(tokens)
+    tokens.forEach(t => console.log("sending to " + t));
+}
