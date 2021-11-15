975ba8d257a9cebacbd8c3f8f3b95d73fb45925e
Alessandro Liparoti <alessandrol@spotify.com>
2021-11-15 10:02:18 +0100

    add generic notification

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const schedulePrematchNofification = require('./schedule_prematch_notification');
 const sendCancellationNotification = require('./send_cancellation_notification');
+const sendGenericNotification = require('./send_generic_notification');
 const insertFakeData = require('./insert_fake_data');
 const admin = require("firebase-admin");
 
@@ -7,3 +8,6 @@ admin.initializeApp();
 
 exports.schedulePrematchNofification = schedulePrematchNofification.schedulePreMatchNotification;
 exports.sendCancellationNotification = sendCancellationNotification.sendCancellationNotification;
+exports.sendGenericNotification = sendGenericNotification.sendGenericNotification;
+
+exports.insertFakeData = insertFakeData.insertFakeData;
