eb79eaf1727c17a0cfaebcbf485c937c75a14677
Alessandro Liparoti <alessandrol@spotify.com>
2022-03-02 14:25:49 +0100

    add get ratings by match function

---
+++ functions-python/ratings/get_ratings_by_match/main.py
@@ -0,0 +1,38 @@
+import asyncio
+
+import firebase_admin
+from google.cloud.firestore_v1 import AsyncClient
+
+firebase_admin.initialize_app()
+
+
+def _get_db():
+    return AsyncClient()
+
+
+def get_ratings_by_match(request):
+    request_json = request.get_json(silent=True)
+    print("data {}".format(request_json))
+
+    request_data = request_json["data"]
+
+    asyncio.run(_get_ratings_by_match(request_data["match_id"]))
+
+    return {"data": {}}, 200
+
+
+async def _get_ratings_by_match(match_id):
+    db = _get_db()
+
+    ratings_doc_ref = db.collection("ratings").document(match_id)
+    ratings_doc = await ratings_doc_ref.get()
+
+    ratings_data = ratings_doc.to_dict()
+
+    response = [(user, list(scores.values())) for user, scores in ratings_data["scores"].items()]
+    return dict(response)
+
+
+if __name__ == '__main__':
+    print(asyncio.run(_get_ratings_by_match("8fmbZokYTfjRDVyGPt1s")))
+
