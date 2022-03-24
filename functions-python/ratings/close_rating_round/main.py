52a831d419d593607ce0fd6ec6fc7844157c1c84
Alessandro Liparoti <alessandrol@spotify.com>
2022-03-24 23:05:53 +0100

    deal with multiple potms

--- functions-python/ratings/close_rating_round/main.py
+++ functions-python/ratings/close_rating_round/main.py
@@ -34,16 +34,20 @@ async def _close_rating_round_firestore(match_id, send_notification=True):
     if match_data["cancelledAt"]:
         raise Exception("Match is canceled")
 
+    if match_data.get("scoresComputedAt", None):
+        raise Exception("Scores already computed")
+
     ratings_doc = await db.collection("ratings").document(match_id).get()
     if not ratings_doc.exists:
         print("No ratings for this match")
-        # mark match as rated and store man_of_the_match
+        # mark match as rated
         await db.collection("matches").document(match_id).set({"scoresComputedAt": timestamp}, merge=True)
         return
 
     scores = ratings_doc.to_dict()["scores"]
 
     # do calculations
+    # user_id -> (avg_score, num_votes)
     final_scores = {}
     for u in scores:
         only_positive = list(filter(lambda s: s > 0, scores[u].values()))
@@ -51,35 +55,44 @@ async def _close_rating_round_firestore(match_id, send_notification=True):
             final_scores[u] = 0
         else:
             s = Decimal(sum(only_positive) / len(only_positive))
-            final_scores[u] = float(round(s, 2))
+            final_scores[u] = (float(round(s, 2)), len(only_positive))
 
-    # who has the biggest dick in the match?        
-    man_of_the_match, man_of_the_match_score = max(final_scores.items(), key=lambda x: x[1])
-    print("final scores {}; man of the match: {}".format(final_scores, man_of_the_match))
+    # who has the biggest dick(s) in the match?
+    scores_sorted = sorted(final_scores.items(), key=lambda x: x[1])
+    scores_sorted.reverse()
+    top_score_and_count = scores_sorted[0][1]
+
+    potm_scores = {}
+    for e in scores_sorted:
+        if e[1] == top_score_and_count:
+            potm_scores[e[0]] = e[1][0]
+    print("final scores {}; man(s) of the match: {}".format(final_scores, potm_scores))
 
     if "isTest" not in match_data or not match_data["isTest"]:
         # store score for users
-        for user, score in final_scores.items():
-            await db.collection("users").document(user).set({"scoreMatches": {match_id: score}}, merge=True)
+        for user, score_and_count in final_scores.items():
+            await db.collection("users").document(user).update({"scoreMatches." + match_id: score_and_count[0]})
 
         # store man of the match info in user doc
-        await db.collection("users").document(man_of_the_match).update({"manOfTheMatch": firestore.firestore.ArrayUnion([match_id])})
+        for user in potm_scores:
+            await db.collection("users").document(user).update({"manOfTheMatch": firestore.firestore.ArrayUnion([match_id])})
 
     # mark match as rated and store man_of_the_match
     await db.collection("matches").document(match_id).set({"scoresComputedAt": timestamp,
-                                                           "manOfTheMatch": {man_of_the_match: man_of_the_match_score}},
+                                                           "manOfTheMatch": potm_scores},
                                                           merge=True)
     if send_notification:
         _send_close_voting_notification(match_id, set(match_data["going"].keys()),
-                                        man_of_the_match, match_data["sportCenterId"])
+                                        set(potm_scores.keys()), match_data["sportCenterId"])
 
 
-def _send_close_voting_notification(match_id, going_users, motm, sport_center_id):
+def _send_close_voting_notification(match_id, going_users, potms, sport_center_id):
     db = firestore.client()
 
     sport_center = db.collection('sport_centers').document(sport_center_id).get().to_dict()["name"]
 
-    going_users.remove(motm)
+    for p in potms:
+        going_users.remove(p)
 
     _send_notification_to_users(
         title="Match stats are available!",
@@ -94,7 +107,7 @@ def _send_close_voting_notification(match_id, going_users, motm, sport_center_id
     _send_notification_to_users(
         title="Congratulations! " + u"\U0001F3C6",
         body="You won the Player of the Match award for the {} match".format(sport_center),
-        users=[motm],
+        users=list(potms),
         data={
             "click_action": "FLUTTER_NOTIFICATION_CLICK",
             "match_id": match_id,
@@ -129,4 +142,4 @@ def _send_notification_to_tokens(title, body, data, tokens):
 
 
 if __name__ == '__main__':
-    print(asyncio.run(_close_rating_round_firestore("gAYBoHYPUmX1GMfCajou", send_notification=True)))
+    print(asyncio.run(_close_rating_round_firestore("EKPi6qHMI2du2sHykRlG", send_notification=False)))
--- functions-python/ratings/reset_ratings_for_match/main.py
+++ functions-python/ratings/reset_ratings_for_match/main.py
@@ -23,7 +23,7 @@ async def _reset_ratings_for_match(match_id):
     match_doc = db.collection("matches").document(match_id)
     match_data = (await match_doc.get()).to_dict()
 
-    potm = list(match_data["manOfTheMatch"].items())[0][0]
+    potms = match_data["manOfTheMatch"].keys()
 
     await match_doc.update({
         "manOfTheMatch": firestore.firestore.DELETE_FIELD,
@@ -32,9 +32,10 @@ async def _reset_ratings_for_match(match_id):
 
     users_going = (await match_doc.get()).to_dict()["going"].keys()
 
-    await db.collection("users").document(potm).update({
-        "manOfTheMatch": firestore.firestore.ArrayRemove([match_id])
-    })
+    for potm in potms:
+        await db.collection("users").document(potm).update({
+            "manOfTheMatch": firestore.firestore.ArrayRemove([match_id])
+        })
 
     for u in users_going:
         try:
@@ -45,4 +46,4 @@ async def _reset_ratings_for_match(match_id):
             print("Failed to update {} user document".format(u))
 
 if __name__ == '__main__':
-    asyncio.run(_reset_ratings_for_match("gIPS0HjiGcpDy1prded0"))
+    asyncio.run(_reset_ratings_for_match("EKPi6qHMI2du2sHykRlG"))
