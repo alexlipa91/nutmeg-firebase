e5e3122fef95753a4c2d22f7f7150651e5761bd1
Alessandro Liparoti <alessandrol@spotify.com>
2022-04-11 20:47:09 +0300

    handle case of missing potms

--- functions-python/ratings/reset_ratings_for_match/main.py
+++ functions-python/ratings/reset_ratings_for_match/main.py
@@ -23,24 +23,25 @@ async def _reset_ratings_for_match(match_id):
     match_doc = db.collection("matches").document(match_id)
     match_data = (await match_doc.get()).to_dict()
 
-    potms = match_data["manOfTheMatch"].keys()
+    if "manOfTheMatch" in match_data:
+        potms = match_data["manOfTheMatch"].keys()
 
-    await match_doc.update({
-        "manOfTheMatch": firestore.firestore.DELETE_FIELD,
-        "scoresComputedAt": firestore.firestore.DELETE_FIELD
-    })
+        await match_doc.update({
+            "manOfTheMatch": firestore.firestore.DELETE_FIELD,
+            "scoresComputedAt": firestore.firestore.DELETE_FIELD
+        })
+
+        for potm in potms:
+            await db.collection("users").document(potm).update({
+                "manOfTheMatch": firestore.firestore.ArrayRemove([match_id])
+            })
 
     users_going = (await match_doc.get()).to_dict()["going"].keys()
-
-    for potm in potms:
-        await db.collection("users").document(potm).update({
-            "manOfTheMatch": firestore.firestore.ArrayRemove([match_id])
-        })
 
     for u in users_going:
         try:
             await db.collection("users").document(u).update({
-            "scoreMatches.{}".format(match_id): firestore.firestore.DELETE_FIELD
+                "scoreMatches.{}".format(match_id): firestore.firestore.DELETE_FIELD
             })
         except:
             print("Failed to update {} user document".format(u))
