5a4499174b1949c1e168232a39eccc8075015625
Alessandro Liparoti <alessandrol@spotify.com>
2022-04-26 11:29:26 +0200

    publish when account is activated

--- functions-python/stripe_connect_webhook/main.py
+++ functions-python/stripe_connect_webhook/main.py
@@ -1,6 +1,7 @@
 import firebase_admin
 import stripe
 from firebase_admin import firestore
+from google.cloud.firestore_v1 import DELETE_FIELD
 
 
 firebase_admin.initialize_app()
@@ -36,10 +37,24 @@ def _exec(request, secret, is_test):
         field_name = "chargesEnabledOnStripe" if not is_test else "chargesEnabledOnStripeTest"
 
         db = firestore.client()
+        user_data = db.collection("users").document(user_id).get().to_dict()
+
+        if user_data.get(field_name, False):
+            print("{} is already set for user {}".format(field_name, user_id))
+            return
+
         db.collection("users").document(user_id).update({
             field_name: True
         })
         print("user {} can now receive payments on stripe".format(user_id))
+
+        for m in user_data["organised_matches" if not is_test else "organised_test_matches"].keys():
+            if db.collection("matches").document(m).get(field_paths=["unpublished_reason"]).to_dict()\
+                    .get("unpublished_reason", None) == "organizer_not_onboarded":
+                print("removing un-publishing blocker because of organizer from match {}".format(m))
+                db.collection("matches").document(m).update({
+                    'unpublished_reason': DELETE_FIELD,
+                })
     else:
         print("event not handled")
 
