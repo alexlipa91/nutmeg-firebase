9afc1cec7b74f2c7bf75e0c2d0caaf6dc144b4db
Alessandro Liparoti <alessandrol@spotify.com>
2022-05-11 10:53:41 +0200

    fix webhook when match does not exist

--- functions-python/stripe_connect_webhook/main.py
+++ functions-python/stripe_connect_webhook/main.py
@@ -23,7 +23,6 @@ def _exec(request):
     sig_header = request.headers['STRIPE_SIGNATURE']
 
     secret = os.environ['STRIPE_WEBHOOK_SECRET']
-    print(secret)
 
     try:
         event = stripe.Webhook.construct_event(payload, sig_header, secret)
@@ -39,29 +38,34 @@ def _exec(request):
 
     # Handle the event
     if event["type"] == "account.updated" and event_data["charges_enabled"]:
-        user_id = event_data["metadata"]["userId"]
-        field_name = "chargesEnabledOnStripe" if not is_test else "chargesEnabledOnStripeTest"
-
-        db = firestore.client()
-        user_data = db.collection("users").document(user_id).get().to_dict()
-
-        if user_data.get(field_name, False):
-            print("{} is already set for user {}".format(field_name, user_id))
-            return
-
-        db.collection("users").document(user_id).update({
-            field_name: True
-        })
-        print("user {} can now receive payments on stripe".format(user_id))
-
-        for m in user_data["created_matches" if not is_test else "created_test_matches"].keys():
-            if db.collection("matches").document(m).get(field_paths=["unpublished_reason"]).to_dict()\
-                    .get("unpublished_reason", None) == "organizer_not_onboarded":
-                print("removing un-publishing blocker because of organizer from match {}".format(m))
-                db.collection("matches").document(m).update({
-                    'unpublished_reason': DELETE_FIELD,
-                })
+        _handle_event(event_data, is_test)
     else:
         print("event not handled")
 
     return {}, 200
+
+
+def _handle_event(event_data, is_test):
+    user_id = event_data["metadata"]["userId"]
+    field_name = "chargesEnabledOnStripe" if not is_test else "chargesEnabledOnStripeTest"
+
+    db = firestore.client()
+    user_data = db.collection("users").document(user_id).get().to_dict()
+
+    if user_data.get(field_name, False):
+        print("{} is already set for user {}".format(field_name, user_id))
+        return
+
+    db.collection("users").document(user_id).update({
+    field_name: True
+})
+    print("user {} can now receive payments on stripe".format(user_id))
+
+    for m in user_data["created_matches" if not is_test else "created_test_matches"].keys():
+        match = db.collection("matches").document(m).get(field_paths=["unpublished_reason"])
+
+        if match.exists and match.to_dict().get("unpublished_reason", None) == "organizer_not_onboarded":
+            print("removing un-publishing blocker because of organizer from match {}".format(m))
+            db.collection("matches").document(m).update({
+                'unpublished_reason': DELETE_FIELD
+            })
