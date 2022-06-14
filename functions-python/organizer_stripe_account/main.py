0598a4e78b3646327f18a0cbb1700af3e1d40a3b
Alessandro Liparoti <alessandrol@spotify.com>
2022-06-14 09:57:05 +0200

    add match metadata in payout

--- functions-python/organizer_stripe_account/main.py
+++ functions-python/organizer_stripe_account/main.py
@@ -153,6 +153,7 @@ def _create_organizer_payout(match_id, attempt):
             amount=amount,
             currency='eur',
             stripe_account=organizer_account,
+            metadata={"match_id": match_id},
         )
         print("payout of {} created: {}".format(amount, payout.id))
         db.collection("matches").document(match_id).update({
