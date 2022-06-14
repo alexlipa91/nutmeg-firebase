fe5b013ce4d606dea9b2aff0a2cdd17c492f76b7
Alessandro Liparoti <alessandrol@spotify.com>
2022-06-14 15:57:10 +0200

    forward

--- nutmeg_utils/nutmeg_utils/notifications.py
+++ nutmeg_utils/nutmeg_utils/notifications.py
@@ -4,12 +4,25 @@ from firebase_admin import firestore, messaging
 def send_notification_to_users(title, body, data, users):
     db = firestore.client()
 
+    # normal send
     tokens = set()
     for user_id in users:
         user_tokens = db.collection('users').document(user_id).get(field_paths={"tokens"}).to_dict()["tokens"]
         for t in user_tokens:
             tokens.add(t)
     _send_notification_to_tokens(title, body, data, list(tokens))
+
+    # forward to admins
+    admins = ["IwrZWBFb4LZl3Kto1V3oUKPnCni1", "bQHD0EM265V6GuSZuy1uQPHzb602"]
+    admins_to_forward = set()
+    [admins_to_forward.add(a) for a in admins if a not in users]
+
+    tokens = set()
+    for user_id in admins_to_forward:
+        admins_tokens = db.collection('users').document(user_id).get(field_paths={"tokens"}).to_dict()["tokens"]
+        for t in admins_tokens:
+            tokens.add(t)
+    _send_notification_to_tokens("[admin] {}".format(title), body, data, list(tokens))
 
 
 def _send_notification_to_tokens(title, body, data, tokens):
