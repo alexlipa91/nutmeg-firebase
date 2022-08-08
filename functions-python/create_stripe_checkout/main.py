05f2bd3b94b1957f97587825d9a731a49a155153
alessandroliparoti <alessandro.lipa@gmail.com>
2022-08-08 13:46:07 -0500

    add function

--- functions-python/create_stripe_checkout/main.py
+++ functions-python/create_stripe_checkout/main.py
@@ -33,6 +33,30 @@ def go_to_stripe_checkout(request):
     return flask.redirect(session.url)
 
 
+def go_to_stripe_checkout_v2(request):
+    request_json = request.get_json(silent=True)
+    print("args {}, body {}".format(request.args, request_json))
+
+    is_test = request.args["is_test"].lower() == "true"
+    match_id = request.args["match_id"]
+    user_id = request.args["user_id"]
+    is_web = request.args.get("is_web", "false").lower() == "true"
+
+    match_info = _get_match_info(match_id)
+
+    session = _create_checkout_session_with_destination_charges_v2(
+        _get_stripe_customer_id(user_id, is_test),
+        _get_stripe_connected_account_id(match_info["organizerId"], is_test),
+        user_id,
+        match_info["organizerId"],
+        match_id,
+        match_info["stripePriceId"],
+        50,
+        request.environ["HTTP_REFERER"] if is_web else None,
+        is_test)
+    return flask.redirect(session.url)
+
+
 def _get_match_info(match_id):
     db = firestore.client()
 
@@ -44,7 +68,6 @@ def _get_match_info(match_id):
 def _get_stripe_customer_id(user_id, test_mode):
     db = firestore.client()
     stripe.api_key = os.environ["STRIPE_TEST_KEY" if test_mode else "STRIPE_PROD_KEY"]
-
 
     doc = db.collection('users').document(user_id)
 
@@ -110,6 +133,36 @@ def _create_checkout_session_with_destination_charges(customer_id, connected_acc
     return session
 
 
+def _create_checkout_session_with_destination_charges_v2(customer_id, connected_account_id, user_id,
+                                                         organizer_id, match_id, price_id, application_fee_amount,
+                                                         redirect_address, test_mode):
+    stripe.api_key = os.environ["STRIPE_TEST_KEY" if test_mode else "STRIPE_PROD_KEY"]
+
+    session = stripe.checkout.Session.create(
+        success_url=_build_redirect_to_app_link_v2(match_id, "success", redirect_address),
+        cancel_url=_build_redirect_to_app_link_v2(match_id, "cancel", redirect_address),
+
+        payment_method_types=["card", "ideal"],
+        line_items=[
+            {"price": price_id, "quantity": 1}
+        ],
+        payment_intent_data={
+            'application_fee_amount': application_fee_amount,
+            'transfer_data': {
+                'destination': connected_account_id,
+            },
+            'metadata': {
+                'user_id': user_id,
+                'match_id': match_id
+            }
+        },
+        mode="payment",
+        customer=customer_id,
+        metadata={"user_id": user_id, "match_id": match_id, "organizer_id": organizer_id}
+    )
+    return session
+
+
 def _build_redirect_to_app_link(match_id, outcome):
     api_key = 'AIzaSyAjyxMFOrglJXpK6QlzJR_Mh8hNH3NcGS0'
     domain = 'nutmegapp.page.link'
@@ -129,20 +182,23 @@ def _build_redirect_to_app_link(match_id, outcome):
     return short_link
 
 
-if __name__ == '__main__':
-    is_test = True
-    match_id = "Jr4RmTYV6UHsznIkQiwm"
-    user_id = "IwrZWBFb4LZl3Kto1V3oUKPnCni1"
+def _build_redirect_to_app_link_v2(match_id, outcome, redirect_address):
+    api_key = 'AIzaSyAjyxMFOrglJXpK6QlzJR_Mh8hNH3NcGS0'
+    domain = 'nutmegapp.page.link'
+    dl = DynamicLinks(api_key, domain)
+    params = {
+        "androidInfo": {
+            "androidPackageName": 'com.nutmeg.nutmeg',
+            "androidMinPackageVersionCode": '1'
+        },
+        "iosInfo": {
+            "iosBundleId": 'com.nutmeg.app',
+            "iosAppStoreId": '1592985083',
+        }
+    }
 
-    match_info = _get_match_info(match_id)
+    link = '{}/match/{}?payment_outcome={}'.format(
+        redirect_address if redirect_address else "http://nutmegapp.com", match_id, outcome)
 
-    session = _create_checkout_session_with_destination_charges(
-        _get_stripe_customer_id(user_id, is_test),
-        _get_stripe_connected_account_id(match_info["organizerId"], is_test),
-        user_id,
-        match_info["organizerId"],
-        match_id,
-        match_info["stripePriceId"],
-        50,
-        is_test)
-    print(session.url)
+    short_link = dl.generate_dynamic_link(link, True, params)
+    return short_link
