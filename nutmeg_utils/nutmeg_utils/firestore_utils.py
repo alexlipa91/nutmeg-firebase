73c75670f439f353b9679a6ba7bc45aa543dac16
alessandroliparoti <alessandro.lipa@gmail.com>
2022-09-12 15:43:54 +0200

    fix serialization function

--- nutmeg_utils/nutmeg_utils/firestore_utils.py
+++ nutmeg_utils/nutmeg_utils/firestore_utils.py
@@ -5,7 +5,7 @@ import google.api_core.datetime_helpers
 
 def _serialize_dates(data):
     for k in data:
-        if type(data[k] == dict):
+        if type(data[k]) == dict:
             data[k] = _serialize_dates(data[k])
         elif type(data[k]) == google.api_core.datetime_helpers.DatetimeWithNanoseconds:
             data[k] = datetime.isoformat(data[k])
