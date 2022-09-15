406cf23357d4e17c3ca709231862df38775bd210
alessandroliparoti <alessandro.lipa@gmail.com>
2022-09-15 10:53:35 +0200

    add get_ratings_by_match_v3

--- functions-python/ratings/get_ratings_by_match/main.py
+++ functions-python/ratings/get_ratings_by_match/main.py
@@ -35,6 +35,18 @@ def get_ratings_by_match_v2(request):
     return {"data": resp}, 200
 
 
+@cross_origin(origins=["*"], allow_headers=["firebase-instance-id-token", "content-type", "authorization"])
+def get_ratings_by_match_v3(request):
+    request_json = request.get_json(silent=True)
+    print("data {}".format(request_json))
+
+    request_data = request_json["data"]
+
+    resp = asyncio.run(_get_ratings_by_match_v3(request_data["match_id"]))
+
+    return {"data": resp}, 200
+
+
 async def _get_ratings_by_match(match_id):
     db = _get_db()
 
@@ -63,5 +75,18 @@ async def _get_ratings_by_match_v2(match_id):
     return ratings_data["scores"]
 
 
+async def _get_ratings_by_match_v3(match_id):
+    db = _get_db()
+
+    ratings_doc_ref = db.collection("ratings").document(match_id)
+    ratings_doc = await ratings_doc_ref.get()
+
+    if not ratings_doc.exists:
+        return {}
+
+    ratings_data = ratings_doc.to_dict()
+    return ratings_data
+
+
 if __name__ == '__main__':
-    print(asyncio.run(_get_ratings_by_match_v2("EKPi6qHMI2du2sHykRlG")))
+    print(asyncio.run(_get_ratings_by_match_v3("4u4zPZ6EGTcAeKOW6j9q")))
