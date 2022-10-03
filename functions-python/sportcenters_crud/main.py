a04db38e08d6a52ab556eb761caa756a00964380
alessandroliparoti <alessandro.lipa@gmail.com>
2022-10-04 07:15:33 +0800

    add function

--- functions-python/sportcenters_crud/main.py
+++ functions-python/sportcenters_crud/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import time
 
 import firebase_admin
 import requests
@@ -96,11 +97,70 @@ def add_user_sportcenter(request):
     return {"data": {}}, 200
 
 
+@cross_origin(origins=["*"], allow_headers=["firebase-instance-id-token", "content-type", "authorization"])
+def add_user_sportcenter_from_place_id(request):
+    request_json = request.get_json(silent=True)
+    print("args {}, data {}".format(request.args, request_json))
+
+    _add_user_sportcenter_from_place_id(
+        request_json["data"]["place_id"],
+        request_json["data"]["additional_info"],
+        request_json["data"]["user_id"],
+    )
+
+    return {"data": {}}, 200
+
+
+def _add_user_sportcenter_from_place_id(place_id, additional_info, user_id):
+    url = "https://maps.googleapis.com/maps/api/place/details/json?place_id={}&fields={}&key={}".format(
+        place_id,
+        "%2C".join(["name", "formatted_address", "geometry", "utc_offset"]),
+        os.environ["GOOGLE_PLACES_API_KEY"]
+    )
+
+    response = requests.request("GET", url, headers={}, data={})
+    result = response.json()["result"]
+
+    lat = result["geometry"]["location"]["lat"]
+    lng = result["geometry"]["location"]["lng"]
+
+    sport_center = {
+        "address": result["formatted_address"],
+        "name": result["name"],
+        "lat": lat,
+        "lng": lng,
+        "timeZonId": _get_timezone_id(lat, lng)
+    }
+    for k in additional_info:
+        sport_center[k] = additional_info[k]
+
+    db = firestore.client()
+
+    db.collection("users").document(user_id).collection("sportCenters") \
+        .document(place_id).set(sport_center)
+
+
+def _get_timezone_id(lat, lng):
+    url = "https://maps.googleapis.com/maps/api/timezone/json?location={}%2C{}&timestamp={}&key={}".format(
+        lat,
+        lng,
+        int(time.time()),
+        os.environ["GOOGLE_PLACES_API_KEY"]
+    )
+    response = requests.request("GET", url, headers={}, data={})
+
+    return response.json()["timeZoneId"]
+
+
 def _get_placeid_info(place_id):
     url = 'https://maps.googleapis.com/maps/api/place/details/json?'
 
     req = requests.get(url + 'place_id=' + place_id + '&key=' + os.environ["GOOGLE_PLACES_API_KEY"]
-                       + "&fields=name%2Cformatted_address%2Cgeometry")
+                       + "&fields=name%2Cformatted_address%2Cgeometry%2Cutc_offset")
     resp = req.json()
 
     return resp['result']
+
+
+if __name__ == '__main__':
+    _get_timezone_id(52.3695049302915,  4.926487980291501)
