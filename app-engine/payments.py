9d9c5d60c7263bc4bd356117d432455ae9af90fd
Alessandro Liparoti <alessandrolipa@meta.com>
2023-04-03 15:30:32 +0200

    restore old way of making payments with dynamic links

--- app-engine/payments.py
+++ app-engine/payments.py
@@ -3,7 +3,6 @@ import stripe
 from firebase_admin import firestore
 from firebase_dynamic_links import DynamicLinks
 from flask import Blueprint
-from flask_cors import cross_origin
 from utils import get_secret
 
 bp = Blueprint('payments', __name__, url_prefix='/payments')
@@ -12,30 +11,38 @@ syncDb = firestore.client()
 
 
 @bp.route("/checkout", methods=["GET"])
-@cross_origin(origins=["*"], allow_headers=["firebase-instance-id-token", "content-type", "authorization"])
 def checkout():
     request = flask.request
     match_id = request.args["match_id"]
     user_id = request.args["user_id"]
+    version = request.args.get("v", 1)
 
     match_info = _get_match_info(match_id)
     is_test = match_info.get("isTest", False)
 
-    session = _create_checkout_session_with_destination_charges_v2(
-        _get_stripe_customer_id(user_id, is_test),
-        _get_stripe_connected_account_id(match_info["organizerId"], is_test),
-        user_id,
-        match_info["organizerId"],
-        match_id,
-        match_info["stripePriceId"],
-        50,
-        is_test)
+    if version == 1:
+        session = _create_checkout_redirects_to_web(
+            _get_stripe_customer_id(user_id, is_test),
+            _get_stripe_connected_account_id(match_info["organizerId"], is_test),
+            user_id,
+            match_info["organizerId"],
+            match_id,
+            match_info["stripePriceId"],
+            50,
+            is_test)
+    else:
+        session = _create_checkout_session_with_deep_links(
+            _get_stripe_customer_id(user_id, is_test),
+            _get_stripe_connected_account_id(match_info["organizerId"], is_test),
+            user_id,
+            match_info["organizerId"],
+            match_id,
+            match_info["stripePriceId"],
+            50,
+            is_test)
+
+
     return flask.redirect(session.url)
-
-
-@bp.route("/success", methods=["GET"])
-def success():
-    return 200, {}
 
 
 def _get_match_info(match_id):
@@ -78,11 +85,9 @@ def _get_stripe_connected_account_id(organizer_id, test_mode):
 
 
 # application_fee_amount includes stripe fees
-def _create_checkout_session_with_destination_charges_v2(customer_id, connected_account_id, user_id,
-                                                         organizer_id, match_id, price_id, application_fee_amount,
-                                                         test_mode):
-    print(get_secret("stripeTestKey" if test_mode else "stripeProdKey"))
-    print(test_mode)
+def _create_checkout_redirects_to_web(customer_id, connected_account_id, user_id,
+                                      organizer_id, match_id, price_id, application_fee_amount,
+                                      test_mode):
     stripe.api_key = get_secret("stripeTestKey" if test_mode else "stripeProdKey")
 
     session = stripe.checkout.Session.create(
@@ -109,7 +114,37 @@ def _create_checkout_session_with_destination_charges_v2(customer_id, connected_
     return session
 
 
-def _build_redirect_to_app_link_v2(match_id, outcome, redirect_address):
+def _create_checkout_session_with_deep_links(customer_id, connected_account_id, user_id,
+                                             organizer_id, match_id, price_id, application_fee_amount,
+                                             test_mode):
+    stripe.api_key = get_secret("stripeTestKey" if test_mode else "stripeProdKey")
+
+    session = stripe.checkout.Session.create(
+        success_url=_build_redirect_to_app_link(match_id, "success", ""),
+        cancel_url=_build_redirect_to_app_link(match_id, "cancel", ""),
+
+        payment_method_types=["card", "ideal"],
+        line_items=[
+            {"price": price_id, "quantity": 1}
+        ],
+        payment_intent_data={
+            'application_fee_amount': application_fee_amount,
+            'transfer_data': {
+                'destination': connected_account_id,
+            },
+            'metadata': {
+                'user_id': user_id,
+                'match_id': match_id
+            }
+        },
+        mode="payment",
+        customer=customer_id,
+        metadata={"user_id": user_id, "match_id": match_id, "organizer_id": organizer_id}
+    )
+    return session
+
+
+def _build_redirect_to_app_link(match_id, outcome, redirect_address):
     api_key = get_secret("dynamicLinkApiKey")
     domain = 'nutmegapp.page.link'
     dl = DynamicLinks(api_key, domain)
@@ -130,9 +165,7 @@ def _build_redirect_to_app_link_v2(match_id, outcome, redirect_address):
     if redirect_address and redirect_address[-1] == "/":
         redirect_address = redirect_address[:-1]
 
-    link = '{}/match/{}?payment_outcome={}'.format(
-        redirect_address if redirect_address else "http://web.nutmegapp.com", match_id, outcome)
-    print(link)
+    link = 'http://web.nutmegapp.com/match/{}?payment_outcome={}'.format(match_id, outcome)
 
     short_link = dl.generate_dynamic_link(link, True, params)
     return short_link
