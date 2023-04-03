2c2b1dca787bc8e2b207033ff030409786c57755
Alessandro Liparoti <alessandrolipa@meta.com>
2023-04-03 16:53:01 +0200

    add token

--- app-engine/users.py
+++ app-engine/users.py
@@ -1,3 +1,4 @@
+import flask
 from firebase_admin import firestore
 from flask import Blueprint
 
@@ -7,9 +8,22 @@ bp = Blueprint('users', __name__, url_prefix='/users')
 syncDb = firestore.client()
 
 
-@bp.route("/<user_id>", methods=["GET"])
+@bp.route("/<user_id>", methods=["GET", "POST"])
 def get_user(user_id):
-    return {"data": _get_user_firestore(user_id)}, 200
+    if flask.request.method == "GET":
+        return {"data": _get_user_firestore(user_id)}, 200
+    elif flask.request.method == "POST":
+        data = flask.request.get_json()
+        syncDb.collection("users").document(user_id).update(data)
+        return {}, 200
+
+
+@bp.route("/<user_id>/tokens", methods=["POST"])
+def add_token(user_id):
+    data = flask.request.get_json()
+    syncDb.collection("users").document(user_id)\
+        .update({"tokens": firestore.firestore.ArrayUnion([data["token"]])})
+    return {}, 200
 
 
 def _get_user_firestore(user_id):
