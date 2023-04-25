b1190cf471ccec0a486b8dedc0443e7dbc5f94d8
Alessandro Liparoti <alessandroliparoti@live.it>
2023-04-25 22:58:16 +0200

    replace usage of secret manager

--- app-engine/stripe_bp.py
+++ app-engine/stripe_bp.py
@@ -1,10 +1,11 @@
+import os
+
 import flask
 
 import stripe
 from flask import Blueprint
 
 from matches import add_user_to_match
-from utils import get_secret
 
 bp = Blueprint('stripe', __name__, url_prefix='/stripe')
 
@@ -15,7 +16,7 @@ def stripe_checkout_webhook():
     event = None
     sig_header = flask.request.headers['STRIPE_SIGNATURE']
 
-    secret = get_secret('stripeCheckoutWebhookSecret' if not is_test else 'stripeCheckoutWebhookTestSecret')
+    secret = os.environ["STRIPE_CHECKOUT_WEBHOOK_SECRET" if not is_test else "STRIPE_CHECKOUT_WEBHOOK_SECRET_TEST"]
 
     try:
         event = stripe.Webhook.construct_event(flask.request.data, sig_header, secret)
