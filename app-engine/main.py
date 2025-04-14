c567975eaebf18988ab083ba0e47573e4fac970c
Alessandro <alessandro.lipa@gmail.com>
2025-04-14 23:42:48 +0200

    make the app start again
    add env local to gitignore
    
    ignore any env
    
    deploy
    
    don't track env local

--- app-engine/main.py
+++ app-engine/main.py
@@ -3,6 +3,11 @@ from firebase_admin import firestore
 
 from src import _create_app
 
+from dotenv import load_dotenv
+
+load_dotenv(".env.local")
+    
+
 def create_app():
     firebase_admin.initialize_app()
     return _create_app(firestore.client())
@@ -15,8 +20,6 @@ if __name__ == "__main__":
     # Used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    from dotenv import load_dotenv
-    load_dotenv("scripts/.env.local")
     app.run(host="localhost", port=8080, debug=True)
 
 
